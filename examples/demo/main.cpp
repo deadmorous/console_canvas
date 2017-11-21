6f1b994a4d03eb9b6cd46efdf6f94a4b05e493e7
Stepan Orlov <my@email.com>
2017-11-21 15:46:01 +0300

    Implemented ctm::Factory and ctm::FactoryMixin.

--- examples/demo/main.cpp
+++ examples/demo/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 
 #include "console_canvas/Box.h"
+#include "console_canvas/Shape.h"
 
 using namespace std;
+using namespace ctm;
+
+class Kaka :
+    public Shape,
+    public FactoryMixin<Kaka, Shape>
+{
+public:
+    Kaka() { cout << "Kaka::Kaka" << endl; }
+    ~Kaka() { cout << "Kaka::~Kaka" << endl; }
+    void paint(Canvas&) const override {}
+    Box boundingBox() const override { return Box(); }
+    void write(std::ostream& s) const override {}
+    void read(std::istream& s) override {}
+};
+
+CTM_REGISTER_TYPE(Kaka, "kaka")
+// static Kaka::Registrator TheKakaRegistrator("kaka");
 
 int main()
 {
@@ -11,7 +29,9 @@ int main()
     using namespace ctm;
     Box b;
     b << Point(1,2) << Point(10,5);
-    cout << b.size() << endl;
+
+    auto x = Shape::newInstance("kaka");
+
     cout << b << endl;
     cout << "Hello World!" << endl;
     return 0;
--- include/console_canvas/Factory.h
+++ include/console_canvas/Factory.h
@@ -1,16 +1,71 @@
 #ifndef FACTORY_H
 #define FACTORY_H
 
+#include <memory>
+#include <string>
+#include <vector>
+#include <functional>
+#include <map>
+#include <algorithm>
+
 // TODO
 
 template< class Interface >
 class Factory
 {
+public:
+    using InterfacePtr = std::shared_ptr<Interface>;
+    using TypeId = std::string;
+    using Generator = std::function<InterfacePtr()>;
+
+    static InterfacePtr newInstance(TypeId typeId)
+    {
+        return registry().find(typeId)->second();
+    }
+
+    static std::vector<TypeId> registeredTypes()
+    {
+        auto& r = registry();
+        std::vector<TypeId> result(r.size());
+        std::transform(r.begin(), r.end(), result.begin(), [](
+                       const typename Registry::value_type& v) -> TypeId {
+            return v.first;
+        });
+    }
+
+    static void registerType(TypeId typeId, Generator generator)
+    {
+        registry()[typeId] = generator;
+    }
+
+private:
+    using Registry = std::map<TypeId, Generator>;
+    static Registry& registry() {
+        static Registry registry;
+        return registry;
+    }
 };
 
 template< class Type, class Interface >
 class FactoryMixin
 {
+public:
+    static std::shared_ptr<Interface> newInstance() {
+        return std::make_shared<Type>();
+    }
+
+    class Registrator
+    {
+    public:
+        Registrator(const typename Type::TypeId& typeId) {
+            Type::registerType(
+                typeId,
+                &FactoryMixin<Type, Interface>::newInstance);
+        }
+    };
 };
+
+#define CTM_REGISTER_TYPE(Type, typeId) \
+    static Type::Registrator The##Type##Registrator(typeId);
 
 #endif // FACTORY_H
