380e46c120aaadf5eed98ff486d354da56caa73c
Leonid Polishchuk <polishchuk13wpp@gmail.com>
2017-12-19 14:34:51 +0300

    circle and polygon

--- src/shapes/Circle.cpp
+++ src/shapes/Circle.cpp
@@ -1,18 +1,62 @@
 #include "console_canvas/shapes/Circle.h"
+#include <ostream>
+
 
 namespace ctm {
 namespace shapes {
 
-void Circle::paint(Canvas&) const
+
+void Circle::paint(Canvas& c) const
 {
-    ASSERT(false); // TODO LP
+    int x0 = center.x();
+    int y0 = center.y();
+
+    int x = radius;
+    int y = 0;
+    int dx = 1;
+    int dy = 1;
+    int err = dx - (radius << 1);
+
+    while (x >= y)
+    {
+        //! Функция изменения одномерного массива, находится в холсте
+        c.setPixel(ctm::Point(x0+x,y0 + y),ctm::Color('*'));
+        c.setPixel(ctm::Point(x0 + y, y0 + x),ctm::Color('*'));
+        c.setPixel(ctm::Point(x0 - y, y0 + x),ctm::Color('*'));
+        c.setPixel(ctm::Point(x0 - x, y0 + y),ctm::Color('*'));
+        c.setPixel(ctm::Point(x0 - x, y0 - y),ctm::Color('*'));
+        c.setPixel(ctm::Point(x0 - y, y0 - x),ctm::Color('*'));
+        c.setPixel(ctm::Point(x0 + y, y0 - x),ctm::Color('*'));
+        c.setPixel(ctm::Point(x0 + x, y0 - y),ctm::Color('*'));
+
+        if (err <= 0)
+        {
+            y++;
+            err += dy;
+            dy += 2;
+        }
+        if (err > 0)
+        {
+            x--;
+            dx += 2;
+            err += dx - (radius << 1);
+        }
+    }
 }
+
 
 Box Circle::boundingBox() const
 {
-    ASSERT(false); // TODO LP
-    return Box();
+    Box b;
+    b.setOrigin(center);
+    b.setWidth(abs((center.x()+radius)-(center.x()-radius)));
+    b.setHeight(abs((center.y()+radius)-(center.y()-radius)));
+    return b;
 }
 
 } // namespace shapes
 } // namespace ctm
+
+
+
+
--- src/shapes/Polygon.cpp
+++ src/shapes/Polygon.cpp
@@ -1,18 +1,88 @@
 #include "console_canvas/shapes/Polygon.h"
-
+#include <iostream>
+#include <math.h>
 namespace ctm {
 namespace shapes {
+# define EPSILON 0.95f
 
-void Polygon::paint(Canvas&) const
+void Polygon::paintLine(Point p1, Point p2, Canvas &c) const
 {
-    ASSERT(false); // TODO LP
+    float phi= atan2(p2.x()-p1.x(),p2.y()-p1.y());
+
+
+    std::vector<int> x;
+    std::vector<int> y;
+
+    if(abs(p2.x()-p1.x())<c.getCanvas_size().width())
+    {
+        if(sin(phi)>0 )
+        {
+            for(int i=0;i<abs(p2.x()-p1.x() );++i)
+            {
+
+                x.push_back(p1.x()+i);
+                int fy=  int( (float(x[i]-p1.x()))*(float(p2.y()-p1.y())) /float(p2.x()-p1.x()) ) +p1.y();
+                y.push_back(fy);
+            }
+        }
+        if(sin(phi)<0 )
+        {
+            for(int i=0;i<abs(p2.x()-p1.x() );++i)
+            {
+
+                x.push_back(p1.x()-i);
+                int fy=  int( (float(x[i]-p1.x()))*(float(p2.y()-p1.y())) /float(p2.x()-p1.x()) ) +p1.y();
+                y.push_back(fy);
+            }
+        }
+
+
+    }
+    else if(abs(p2.x()-p1.x())>c.getCanvas_size().width())
+    {
+
+    }
+
+    for(int i=0;i<x.size();++i)
+    {
+        ctm::Point p (x[i], y[i]);
+        c.setPixel(p,ctm::Color('*') );
+    }
+
 }
+
+
+
+void Polygon::paint(Canvas& c) const
+{
+    Point first;
+    Point end;
+
+    for(int i=0;i<points.size();++i)
+    {
+        first=points[i];
+        end=points[i+1];
+
+        paintLine(first,end,c);
+        first=end;
+    }
+
+}
+
+
 
 Box Polygon::boundingBox() const
 {
-    ASSERT(false); // TODO LP
-    return Box();
+    Box b;
+
+
+     b.setOrigin(ctm::Point( points[0].x(),points[0].y()));
+
+     b.setWidth(abs((points[points.size()-1].x()-points[0].x())));
+     b.setHeight(abs((points[points.size()-1].y()-points[0].y())));
+    return b;
 }
 
 } // namespace shapes
 } // namespace ctm
+
